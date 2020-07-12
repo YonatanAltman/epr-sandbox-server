6b4e7e13ce21ba3a8340f361cc99ab0c6e01731c
‘Yonatan <yonatan@yaltman.com>
2020-07-12 18:07:26 +0300

    print all users

--- SandBox.BL/UserManager.vb
+++ SandBox.BL/UserManager.vb
@@ -1,7 +1,20 @@
-﻿Public Class UserManager
-    Shared UserList As List(Of User)
+﻿Imports SandBox.Entities
 
+Public Class UserManager
+    Shared Function GerUserList() As List(Of User)
+        Dim list As New List(Of User)
 
+        list.Add(New User(1001, "Yonatan"))
+        list.Add(New User(1002, "Maayan"))
+        list.Add(New User(1003, "Shirit"))
+        list.Add(New User(1004, "Uri"))
+
+        Return list
+    End Function
+
+    Public Function GetAll() As List(Of User)
+        Return GerUserList()
+    End Function
 
 
 End Class
--- Tester/Module1.vb
+++ Tester/Module1.vb
@@ -1,7 +1,33 @@
-﻿Module Module1
+﻿Imports SandBox.BL
+Imports SandBox.Entities
+
+Module Module1
 
     Sub Main()
+        Console.WriteLine("Let's start with your name:")
+        Dim msg = "Thank you :"
+        Dim name = Console.ReadLine()
+        msg &= name
+
+        Console.WriteLine(msg)
+
+        Dim list = GetAllUsers()
+
+        For Each user In list
+            Console.WriteLine(user.Name)
+        Next
+
+
+
 
     End Sub
+
+
+    Function GetAllUsers() As List(Of User)
+        Dim manager = New UserManager()
+        Return manager.GetAll()
+
+    End Function
+
 
 End Module
