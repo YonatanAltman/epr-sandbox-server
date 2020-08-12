6f8be629fb425a1a37fcc3a92deee57a9742a25b
Shirit <shiritl869@gmail.com>
2020-08-12 09:14:13 +0300

    my first area

---
+++ Sandbox.Api/Controllers/AreaController.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class AreaController
+        Inherits ApiController
+
+        ' GET: api/Area
+        Public Function GetValues() As IEnumerable(Of String)
+            Return New String() {"value1", "value2"}
+        End Function
+
+        ' GET: api/Area/5
+        Public Function GetValue(ByVal id As Integer) As String
+            Return "value"
+        End Function
+
+        ' POST: api/Area
+        Public Sub PostValue(<FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' PUT: api/Area/5
+        Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' DELETE: api/Area/5
+        Public Sub DeleteValue(ByVal id As Integer)
+
+        End Sub
+    End Class
+End Namespace
